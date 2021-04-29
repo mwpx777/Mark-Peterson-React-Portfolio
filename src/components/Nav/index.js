d20b63a8392a9762012c1fbdcd109ddb17bdd128
MWPX777 <mwpx777@gmail.com>
2021-04-29 18:55:01 -0500

    removed home

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -22,11 +22,11 @@ function Nav(props) {
 
             <div className="navbarCenter">
                 <ul className="navbar-nav">
-                    <li className="nav-item" >
+                    {/* <li className="nav-item" >
                         <a className="nav-link" id="navLink" href="/index.html">
                             Home
                         </a>
-                    </li>
+                    </li> */}
 
                     {categories.map((category) => {
 
